3e38bede2c871697f56ef4dd4b96ee41ff25833d
jonny <joniamhandy@gmail.com>
2023-04-16 20:54:19 +0200

    using tailwind now
    god i love it i should have used it earlier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,150 +1,25 @@
-<header>
-  <nav>
-    <a href="/" id="homelink">
-      <div id="titlediv">
-        <img src="favicon.png" alt="logo" />
-        <p id="title">InnateMC</p>
+<script>
+  import "../app.css";
+</script>
+
+<header class="decoration-clone z-40 content-center flex text-center">
+  <nav class="flex z-40 w-screen text-white h-14 backdrop-blur-xl backdrop-saturate-150 fixed justify-between text-center bg-[#1a191cbc] shadow-sm shadow-[#282828]">
+    <a href="/" id="homelink" class="h-14">
+      <div class="cursor-pointer text-xl font-bold flex flex-row h-full items-center ml-4">
+        <img src="favicon.png" alt="logo" class="w-6 object-contain" />
+        <p class="ml-4">InnateMC</p>
       </div>
     </a>
 
-    <div id="links">
-      <a href="news">News</a>
-      <a href="docs">Docs</a>
-      <a href="https://github.com/InnateMC/InnateMC/releases">Releases</a>
-      <a href="https://github.com/InnateMC/InnateMC">Contribute</a>
+    <div class=" flex h-full items-center pr-4 font-sans">
+      <a href="news" class="w-24 text-center text-xs hover:text-[#2997ff]">News</a>
+      <a href="docs" class="w-24 text-center text-xs hover:text-[#2997ff]">Docs</a>
+      <a href="https://github.com/InnateMC/InnateMC/releases" class="w-24 text-center text-xs hover:text-[#2997ff]">Releases</a>
+      <a href="https://github.com/InnateMC/InnateMC" class="w-24 text-center text-xs hover:text-[#2997ff]">Contribute</a>
     </div>
   </nav>
 </header>
-<div style="position:absolute; left:0; top:0; width:100%; height:100%; overflow:hidden;">
-  <img src="logo.png" id="background" alt="background" />
+<div class="-z-10 absolute left-0 top-0 w-full h-full overflow-hidden">
+  <img src="logo.png" alt="background" class="absolute top-0 blur-xs h-96 w-96 saturate-150 animate-randommovement overflow-hidden select-none" />
 </div>
 <slot />
-
-<style>
-  #homelink {
-    text-decoration: none;
-  }
-  header {
-    z-index: 4;
-  }
-  nav {
-    z-index: 3;
-    width: calc(100%);
-    color: #f5f5f7;
-    height: 52px;
-    backdrop-filter: blur(20px) saturate(180%);
-    -webkit-backdrop-filter: blur(20px) saturate(180%);
-
-    position: fixed;
-    font-family: "SF Pro Display", "SF Pro Icons", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    background-color: rgba(29, 29, 31, 0.72);
-    box-shadow: 0 1px 0 0 hsla(0, 0%, 100%, 0.24);
-  }
-  #title {
-    width: 80px;
-    font-weight: 600;
-    color: #fff;
-    text-decoration: none;
-    cursor: pointer;
-    font-size: 1.2em;
-    text-align: center;
-  }
-
-  #links {
-    display: flex;
-    justify-content: space-evenly;
-    padding-right: 15px;
-  }
-  @media only screen and (max-width: 600px) {
-    #links {
-      flex-direction: column;
-    }
-    nav {
-      flex-direction: column;
-      align-items: start;
-    }
-    #titlediv {
-      justify-self: start;
-      height: 52px;
-    }
-  }
-  #links a {
-    width: 80px;
-    font-weight: 400;
-    color: #fff;
-    text-decoration: none;
-    cursor: pointer;
-    font-size: 0.8em;
-    text-align: center;
-  }
-  #links a:hover {
-    color: #2997ff;
-  }
-  #background {
-    position: absolute;
-    top: 0px;
-    -webkit-filter: blur(120px) saturate(180%);
-    height: 500px;
-    width: 500px;
-    animation: moverandomly 100s infinite ease-in-out;
-    -webkit-animation: webkit-moverandomly 100s infinite ease-in-out;
-    overflow: hidden;
-    user-select: none;
-    -webkit-user-drag: none;
-  }
-  @keyframes moverandomly {
-    0% {
-      transform: translate(0%, 0%) scale(1);
-    }
-    30% {
-      transform: translate(20%, 100%) scale(1.3);
-    }
-    50% {
-      transform: translate(40%, 80%) scale(1);
-    }
-    70% {
-      transform: translate(100%, 0%) scale(3);
-    }
-    90% {
-      transform: translate(20%-20%) scale(2);
-    }
-    100% {
-      transform: translate(0%, 0%) scale(1);
-    }
-  }
-  @-webkit-keyframes webkit-moverandomly {
-    0% {
-      transform: translate(0%, 0%) scale(1);
-    }
-    30% {
-      transform: translate(20%, 100%) scale(1.3);
-    }
-    50% {
-      transform: translate(40%, 80%) scale(1);
-    }
-    70% {
-      transform: translate(100%, 0%) scale(3);
-    }
-    90% {
-      transform: translate(20%-20%) scale(2);
-    }
-    100% {
-      transform: translate(0%, 0%) scale(1);
-    }
-  }
-  #titlediv {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  #titlediv img {
-    height: 30px;
-    width: 30px;
-    padding-left: 15px;
-    padding-right: 15px;
-  }
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,10 @@
-<main>
-  <div id="catchphrase">
-    <img src="logo.png" id="logo" alt="logo" />
-    <h5>InnateMC for macOS</h5>
-    <h3>The Minecraft Launcher we know you have been searching for</h3>
+<main class="pt-10 flex content-center z-2 h-screen">
+  <div id="catchphrase" class="flex justify-center align-middle flex-col text-center w-full font-sans text-white ml-10">
+    <img src="logo.png" id="logo" alt="logo" class="self-center w-32" />
+    <h5 class="text-2xl">InnateMC for macOS</h5>
+    <h3 class="text-6xl mt-8">The Minecraft Launcher we know you have been searching for</h3>
+  </div>
+  <div class="overflow-hidden">
+    <img src="screenshot.png" alt="screenshot" class="h-full object-cover object-left" />
   </div>
 </main>
-
-<style>
-  main {
-    padding-top: 100px;
-    display: flex;
-    justify-content: center;
-    z-index: 2;
-  }
-
-  #catchphrase {
-    font-size: 40px;
-    line-height: 1.1;
-    font-weight: 600;
-    letter-spacing: 0em;
-    font-family: "SF Pro Display", "SF Pro Icons", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    color: white;
-    z-index: 2;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    text-align: center;
-  }
-
-  #logo {
-    width: 130px;
-    aspect-ratio: 1/1;
-  }
-</style>
