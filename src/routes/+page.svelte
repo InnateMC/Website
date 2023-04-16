b67824a618cf9e3a09f49de03c2a845266c55fcc
jonny <joniamhandy@gmail.com>
2023-04-16 15:42:02 +0200

    first things
    naming conventions are hell, ill fix it later™

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<main>
+  <div id="catchphrase">
+    <img src="logo.png" id="logo" alt="logo" />
+    <h5>InnateMC for macOS</h5>
+    <h3>The Minecraft Launcher we know you have been searching for</h3>
+  </div>
+</main>
+
+<style>
+  main {
+    padding-top: 100px;
+    display: flex;
+    justify-content: center;
+    z-index: 2;
+  }
+
+  #catchphrase {
+    font-size: 40px;
+    line-height: 1.1;
+    font-weight: 600;
+    letter-spacing: 0em;
+    font-family: "SF Pro Display", "SF Pro Icons", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    color: white;
+    z-index: 2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  #logo {
+    width: 130px;
+    aspect-ratio: 1/1;
+  }
+</style>
